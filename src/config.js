827919e6833ade10e57d75ba563005d08e7c95b0
Yaroslav Grogul <time7bass@gmail.com>
2023-06-13 22:20:24 +0200

    debug edit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const backendURL = process.env.BACKEND_URL;
+// const backendURL = process.env.BACKEND_URL;
 // const backendURL = 'http://localhost:3000';
-// const backendURL = 'http://152.67.72.136:7000';
+const backendURL = 'http://152.67.72.136:7000';
 
 module.exports = backendURL;
